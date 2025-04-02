f499c07f7fdab2bd1153596f2b70086e23c2052a
Seunghoon Shin <liante0904@gmail.com>
2025-04-02 19:39:14 +0900

    vite.config.js의 publicDir 주석 수정 및 netlify.toml 파일 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public', // 🔥 public 폴더를 dist로 복사
+  publicDir: 'public', // ✅ public 폴더 유지
 })
